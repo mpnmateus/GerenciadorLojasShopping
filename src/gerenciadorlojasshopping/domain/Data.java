0a23af49db277908d7e6da20230a84184e2455c4
Mateus Novo <mpnmateus@gmail.com>
2024-05-10 19:04:40 -0300

    Incluindo novos métodos no construtor para checagem de data: refatorado e completo!

--- src/gerenciadorlojasshopping/domain/Data.java
+++ src/gerenciadorlojasshopping/domain/Data.java
@@ -4,30 +4,62 @@ public class Data {
     private int dia, mes, ano;
 
     public Data(int dia, int mes, int ano){
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
-        if (dia == 31 && mes == 2 || dia == 31 && mes == 4 || dia == 31 && mes == 6 ||
-                dia == 31 && mes == 9 || dia == 31 && mes == 11) {
-            System.out.println("Erro: o mês preenchido não pode ter 31 dias.");
+
+        if (validarData(dia, mes, ano)){
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        } else {
+            System.out.println("Data inválida! Alterando para a data padrão: 1/1/2000");
             this.dia = 1;
             this.mes = 1;
             this.ano = 2000;
         }
-        if (dia == 29 && mes == 2 && verificaAnoBissexto() == false){
-            System.out.println("Erro: não é possível haver 29/02, pois o ano não é bissexto");
-            this.dia = 1;
-            this.mes = 1;
-            this.ano = 2000;
+    }
+    
+    private boolean validarData(int dia, int mes, int ano){
+        if (mes < 1 || mes > 12){
+            return false;
         }
+        if ( dia <1 || dia > diasNoMes(mes, ano)){
+            return false;
+        }
+        return true;
+    }
+
+    private int diasNoMes(int mes, int ano){
+        switch (mes){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2:
+                if (verificaAnoBissexto() == true){
+                    return 29;
+                } else {
+                    return 28;
+                }
+            default:
+                return -1;
+        }
+    }
+
+
+    public boolean verificaAnoBissexto(){
+        return (ano % 4 == 0 && (ano % 100 !=0 || ano % 400 == 0));
     }
     @Override
     public String toString(){
         return "Data: "+this.dia+"/"+this.mes+"/"+this.ano;
-    }
-
-    public boolean verificaAnoBissexto(){
-        return (ano % 4 == 0 && ano % 100 !=0) || (ano % 400 == 0);
     }
 
     public int getDia() { return dia; }
