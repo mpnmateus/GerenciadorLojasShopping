c99d4c7ae76b876410bafaecf06f755a276b5190
Mateus Novo <mpnmateus@gmail.com>
2024-05-17 12:12:13 -0300

    Criado subclasse Cosmetico

---
+++ src/gerenciadorlojasshopping/domain/Cosmetico.java
@@ -0,0 +1,19 @@
+package gerenciadorlojasshopping.domain;
+
+public class Cosmetico extends Loja {
+    private double taxaComercializacao;
+
+    public Cosmetico(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario,
+                     Endereco endereco, Data dataFundacao, double taxaComercializacao) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.taxaComercializacao = taxaComercializacao;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString()+ "\nTaxa de Comercialização: "+this.taxaComercializacao;
+    }
+
+    public double getTaxaComercializacao() { return taxaComercializacao; }
+    public void setTaxaComercializacao(double taxaComercializacao) { this.taxaComercializacao = taxaComercializacao; }
+}
