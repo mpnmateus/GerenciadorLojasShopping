d24f85952f1fdcc44e6128b015d13488e8f85703
Mateus Novo <mpnmateus@gmail.com>
2024-06-10 19:33:31 -0300

    Getters, setters e mÃetodo toString()

--- src/gerenciadorlojasshopping/domain/Shopping.java
+++ src/gerenciadorlojasshopping/domain/Shopping.java
@@ -11,4 +11,20 @@ public class Shopping {
         this.lojas = new Loja[qntMaximaLojas];
     }
 
+    @Override
+    public String toString(){
+        return String.format(
+                "Shopping: \n[Nome = %s, Endereço = %s, \nLojas = %s]",
+                this.nome, this.endereco, this.lojas
+        );
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Endereco getEndereco() { return endereco; }
+    public void setEndereco(Endereco endereco) { this.endereco = endereco; }
+
+    public Loja[] getLojas() { return lojas; }
+    public void setLojas(Loja[] lojas) { this.lojas = lojas; }
 }
