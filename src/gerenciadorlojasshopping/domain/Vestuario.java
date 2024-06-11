4320d2aabd1f1fc323f16e02b6e546649fafeac2
Mateus Novo <mpnmateus@gmail.com>
2024-06-11 11:20:02 -0300

    atualizando construtor para contemplar array

--- src/gerenciadorlojasshopping/domain/Vestuario.java
+++ src/gerenciadorlojasshopping/domain/Vestuario.java
@@ -9,6 +9,13 @@ public class Vestuario extends Loja {
         this.produtosImportados = produtosImportados;
     }
 
+    //Construtor etapa04
+    public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario,
+                     Endereco endereco, Data dataFundacao, boolean produtosImportados, int qntMaximaProdutos) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, qntMaximaProdutos);
+        this.produtosImportados = produtosImportados;
+    }
+
     public String toString(){
         return super.toString() + "\nProdutos importados: " + produtosImportados + "\n";
     }
