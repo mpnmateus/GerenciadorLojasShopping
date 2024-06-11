7061a9cdb2d51a590f572afddf2291f24666f850
Mateus Novo <mpnmateus@gmail.com>
2024-06-11 11:14:51 -0300

    atualizando construtor para contemplar array

--- src/gerenciadorlojasshopping/domain/Cosmetico.java
+++ src/gerenciadorlojasshopping/domain/Cosmetico.java
@@ -9,6 +9,13 @@ public class Cosmetico extends Loja {
         this.taxaComercializacao = taxaComercializacao;
     }
 
+    //Construtor etapa4
+    public Cosmetico(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario,
+                     Endereco endereco, Data dataFundacao, double taxaComercializacao, int qntMaximaProdutos) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, qntMaximaProdutos);
+        this.taxaComercializacao = taxaComercializacao;
+    }
+
     @Override
     public String toString(){
         return super.toString()+ "\nTaxa de Comercialização: "+this.taxaComercializacao;
